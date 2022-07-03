c09d7d4ca68bb47773bace664e73861db2edc27f
Allan Soares Amaral <allan-cebola@hotmail.com>
2022-07-03 13:20:38 -0300

    css flex part 1
    adiciona alguns aprendizados de posicionamento css

--- Pessoal/testes.py
+++ Pessoal/testes.py
@@ -1,9 +1,5 @@
-dados =list()
-dados.append(52)
-dados.append(67)
-dados.append(55)
-dados.append(56)
-dados.append(54)
-
-for i in dados:
-    print(i)
+def x (val):
+    val[0]=45
+val=[2,4,6]
+x(val)
+print(val)
